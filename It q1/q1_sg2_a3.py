2c154d32adf1140504b1f10f61c9e322af820a7c
Judy239-ui <jmvalera2030@clc.pshs.edu.ph>
2026-08-16 23:26:39 +0800

    Add Chinese Zodiac sign calculator
    This script calculates and displays the Chinese Zodiac sign based on the user's birth year, with input validation for years before 1900.

---
+++ It q1/q1_sg2_a3.py
@@ -0,0 +1,29 @@
+year = int(input("Enter your birth year:"))
+difference = year - 1900
+remainder = difference % 12
+if year < 1900:
+    print("Invalid Year, it should be not be earlier than 1900")
+if remainder == 0:
+    print("Your Chinese Zodiac Sign is: Rat (鼠 / Shǔ)")
+elif remainder == 1:
+    print("Your Chinese Zodiac Sign is: Ox(牛 / Niú)")
+elif remainder == 2:
+    print("Your Chinese Zodiac Sign is: Tiger(虎 / Hǔ)")
+elif remainder == 3:
+    print("Your Chinese Zodiac Sign is: Rabbit(兔 / Tù)")
+elif remainder == 4:
+    print("Your Chinese Zodiac Sign is: Dragon(龙 / Lóng)")
+elif remainder == 5:
+    print("Your Chinese Zodiac Sign is: Snake(蛇 / Shé)")
+elif remainder == 6:
+    print("Your Chinese Zodiac Sign is: Horse(马 / Mǎ)")
+elif remainder == 7:
+    print("Your Chinese Zodiac Sign is: Goat(羊 / Yáng)")
+elif remainder == 8:
+    print("Your Chinese Zodiac Sign is: Monkey (猴 / Hóu)")
+elif remainder == 9:
+    print("Your Chinese Zodiac Sign is: Rooster (鸡 / Jī)")
+elif remainder == 10:
+    print("Your Chinese Zodiac Sign is: Dog (狗 / Gǒu)")
+elif remainder == 11:
+    print("Your Chinese Zodiac Sign is: Pig(猪 / Zhū)")
